302d0443546dcb00394d7095febb966e4d0dd100
Arabella9104 <ArabellaA697@Stemcollegiate.ca>
2025-02-13 09:26:38 -0700

    Create Feb13.py

---
+++ Feb13.py
@@ -0,0 +1,3 @@
+# for day 1 i learn how to make a comment and how to make a code on Replit 
+print("hello Replit")
+print("""I am signing up for Replits's 100 days of Python challenge! I will make sure to spend some time everyday coding along, for a minimum of 10 minutes a day.I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happened to be. No excuses for not coming from the middle of a field!""")
